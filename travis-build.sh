b98a02dc4c8bb2689db2a66bc347ffe95b751ef2
Uri Herrera <hello@uriherrera.com>
2021-11-09 23:01:37 -0600

    update pkg

--- travis-build.sh
+++ travis-build.sh
@@ -26,7 +26,7 @@ mk-build-deps -i -t "apt-get --yes" -r
 
 ### Clone repo.
 
-git clone --single-branch --branch v2.0 https://invent.kde.org/maui/clip.git
+git clone --single-branch --branch v2.1 https://invent.kde.org/maui/clip.git
 
 mv clip/* .
 
